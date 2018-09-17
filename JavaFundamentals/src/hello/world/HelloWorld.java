da7f6c2ad3ccdfbeb8cf20323cfcbd8d453fb3b6
Test <Test@DIDI.certivox.local>
2018-09-17 19:44:04 +0300

    Initial commint Hello World

---
+++ JavaFundamentals/src/hello/world/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello.world;
+
+/** 
+ * The HelloWorld class implements an application that
+ * simply displays "Hello World!" to the standard output.
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
