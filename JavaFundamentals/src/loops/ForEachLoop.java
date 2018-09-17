3798d47272701d982b62d83e88b6917102dc39b5
Test <Test@DIDI.certivox.local>
2018-09-17 23:18:00 +0300

    For each loop

---
+++ JavaFundamentals/src/loops/ForEachLoop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class ForEachLoop {
+	public static void main(String[] args) {
+		String[] months = {"January", "February", "March", "April", "May", "June", "July","Augist", "September", "October", "November", "December"};
+		for (String i :  months) {
+			System.out.println(i);
+		}
+	}
+}
